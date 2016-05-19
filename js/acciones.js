39d5b89ea19561a33396794b48bc7742cf303932
rojanomartinez <jesus.3fdsaupoo@gmail.com>
2016-05-19 14:38:01 -0500

    ybkjmjm

--- js/acciones.js
+++ js/acciones.js
@@ -5,14 +5,14 @@ $(document).ready(function(e) {
 		
 		$('#localizar').on('tap', function() {
 			navigator.geolocation.getCurrentPosition( function (position){
-				$('#localizacion').html('Latitude: ' + position.coords.latitude + '\n' +
+				$('#localizacion').html('Latitude: '       + position.coords.latitude          + '\n' +
 				'Longitude:'         + position.coords.longitude         + '\n' +
 				'Altitude:'          + position.coords.altitude          + '\n' +
 				'Accuracy:'          + position.coords.accuracy          + '\n' +
 				'Altitude Accuracy:' + position.coords.altitudeAccuracy  + '\n' +
 				'Heading:'           + position.coords.heading           + '\n' +
 				'Speed:'             + position.coords.speed             + '\n' +
-				'Timestamp:'         + position.coords.timestamp         + '\n');
+				'Timestamp:'         + position.timestamp                + '\n');
 			}, $('#localizacion').html('Error en la localizacion') );
 		});
 		
