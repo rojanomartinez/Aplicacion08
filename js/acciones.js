0029a332fc5b0c652bf2029c4dc639f52038d734
rojanomartinez <jesus.3fdsaupoo@gmail.com>
2016-05-19 14:28:41 -0500

    now?

--- js/acciones.js
+++ js/acciones.js
@@ -3,7 +3,7 @@ $(document).ready(function(e) {
     document.addEventListener("deviceready", onDeviceReady, false);
 	function onDeviceReady() {
 		
-		$('localizar').on('tap', function() {
+		$('#localizar').on('tap', function() {
 			navigator.geolocation.getCurrentPosition( function (position){
 				$('#localizacion').html('Latitude: ' + position.coords.latitude + '\n' +
 				'Longitude:'         + position.coords.longitude         + '\n' +
